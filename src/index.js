04490dd39db9cd64993008f64a250f70386db5f8
sanketrakshe <sanketrakshe07@gmail.com>
2023-10-12 16:51:05 +0530

    Created nodemailer transport object and tried (Using nodemailer npm Package)

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ const { ServerConfig } = require('./config');
 
 const apiRoutes = require('./routes');
 
+const mailsender = require('./config/email-config')
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);  
 
-app.listen(ServerConfig.PORT , () => {
+app.listen(ServerConfig.PORT , async () => {
     console.log(`Successfully started a server on PORT : ${ServerConfig.PORT}`);
-    //logger.info("Successfully started the server");
+    try {
+        const response = await mailsender.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: 'sanketrakshe07@gmail.com',
+            subject: 'Is the service working ? now as well',
+            text: 'Yes it is working'
+        });
+        console.log(response);
+    } catch(error) {
+        console.log(error);
+    }
 });
